bfdf6d55553971f618689f6173f0db0d668f602d
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-04-15 20:39:00 +0530

    added

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// root
+// root dom
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
